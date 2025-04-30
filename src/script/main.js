4fea4ce51d3822bdc9f53f582e9ceb78b70166e6
SDB <91771020+Sagarkhatarkar12@users.noreply.github.com>
2025-04-30 22:51:52 +0530

    adding delele logic on our todo list

--- src/script/main.js
+++ src/script/main.js
@@ -1,48 +1,63 @@
 let Category = document.querySelector("#CateogroySelection");
 let inputText = document.querySelector("#inputText");
- let textArea = document.querySelector("#textArea");
- let messageContainer = document.querySelector("#messageContainer");
- let inputTextValue;
- let collection_item = [];
- let textAreaValue;
- let submitButton = document.querySelector("#submit");
- submitButton.addEventListener("click",(e)=>{
+let textArea = document.querySelector("#textArea");
+let messageContainer = document.querySelector("#messageContainer");
+let inputTextValue;
+let textAreaValue;
+let collection_item = []
+let submitButton = document.querySelector("#submit");
+submitButton.addEventListener("click", (e) => {
     e.preventDefault();
 
-    inputTextValue = inputText.value;
-    textAreaValue = textArea.value;
-    let categoryValue = Category.value;
-   let object = {
-    "categoryValue"  : categoryValue,
-    "inputTextValue" :   inputTextValue,
-      "textAreaValue":  textAreaValue
-    
-    }
-    collection_item.push(object);
-    console.log(inputTextValue,textAreaValue)
+    inputTextValue = inputText.value.trim();
+    textAreaValue = textArea.value.trim();
+    let categoryValue = Category.value.trim();
+    collection_item.push({ "inputTextValue": inputTextValue, "textAreaValue": textAreaValue })
+    localStorage.setItem("collection_item", JSON.stringify(collection_item));
+    // localStorage.setItem("category",categoryValue);
+    // localStorage.setItem("inputText",inputTextValue);
+    // localStorage.setItem("textArea",textAreaValue);
 
-    if(  inputTextValue == "" || textAreaValue == ""){
+
+    console.log(inputTextValue, textAreaValue)
+
+    if (inputTextValue == "" || textAreaValue == "") {
         alert("some error try again later");
         return;
     }
-    else{
+    else {
 
-  
-    inputText.value = "";
-    textArea.value = "";
-    Category.value = "";
+
+        inputText.value = "";
+        textArea.value = "";
+        Category.value = "";
+        render();
+    }
+
+
+
+})
+function deleteItem(index) {
+
+    collection_item = localStorage.getItem("collection_item");
+
+    collection_item = JSON.parse(collection_item) || [];
+    collection_item.splice(index, 1);
+    localStorage.setItem("collection_item", JSON.stringify(collection_item));
+
     render();
 }
+render();
+function render() {
+    messageContainer.innerHTML = "";
+    collection_item = localStorage.getItem("collection_item");
+    collection_item = JSON.parse(collection_item);
+    // console.log(collection_item);
 
-        
+    collection_item.forEach((item, index) => {
+        console.log(index);
 
- })
- function render(){
-     messageContainer.innerHTML="";
-    collection_item.forEach((item,index)=>{
-      console.log(index);
-      
-      messageContainer.innerHTML+=` <div class="container-todo-list-message">
+        messageContainer.innerHTML += ` <div class="container-todo-list-message">
                     <div class="container-todo-list-message-checkBox">
                         <input class="container-todo-list-message-heading-checkBox-value" type="checkbox" name="" id="">
 
@@ -52,10 +67,10 @@ let inputText = document.querySelector("#inputText");
                         <p>${item.textAreaValue}</p>
                     </div>
                     <div class="container-todo-list-message-delete">
-                        <button class="container-todo-list-message-delete-value"><i class="ri-delete-bin-line"></i></button>
+                        <button onclick=(deleteItem(${index})) class="container-todo-list-message-delete-value"><i class="ri-delete-bin-line"></i></button>
                 </div>
             </div>
 
             `
     })
- }
+}
